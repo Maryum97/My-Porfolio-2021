5fc9a55137b307e7eb8470386e8c6d97c5b8e87a
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 12:41:19 +0100

    random edit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path='/' component={About}/>
-        <Route exact path='/porfolio' component={Portfolio}/>
+        <Route exact path='/portfolio' component={Portfolio}/>
         <Route exact path='/contact' component={Contact}/>
       </div>
     </Router>
