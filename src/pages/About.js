9d87cecccc2d7e1a1d5794acec02393a95e93728
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 15:08:54 +0100

    added page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,8 @@ function About() {
                             <h2>Introduction</h2>
                             <p>
                                 I, Maryum Bokhari, am a student currently working on a venture to become a Full-Stack Web Developer. I am a hardworking and determined individual, working my way to set a successful career in engineering and web development.
-                                This website provides details about my life, hobbies and ambitions that you may find interesting and inspiring. For more information about my lifestyle and background, have a look at the images in the <a href="/portfolio">Porfolio</a> section.
+                                This website provides details about my life, hobbies and ambitions that you may find interesting and inspiring. For more information about my lifestyle and background, have a look at the images in the <a href="/portfolio">Porfolio</a> section.<br></br>
+                                Originally from Pakistan, I have achieved my basic education from my hometown, Lahore, and then moved to England in 2016 to continue my pursuit in becoming an engineer, from Aston University. In 2020, I joined a Bootcamp at University of Birmingham, to learn and become an expert at coding.<br></br>
                             </p>
                         </Col>
                     </Row>
