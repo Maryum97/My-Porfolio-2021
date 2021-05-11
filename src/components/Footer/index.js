0d5711d2e4ed5b26688c182440922110f7ea08f7
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 15:06:47 +0100

    imported Font Awesome

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import { Container, Row, Col } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer() {
     return (
@@ -9,9 +10,9 @@ function Footer() {
                 <Row>
                     <Col>© 2021 Copyright</Col>
                     <Col>
-                        Follow me on social media:
-                            <a href="https://www.linkedin.com/in/maryum-bokhari-9070581ba/" target="_blank"><i class="fab fa-linkedin"></i></a>
-                            <a href="https://www.instagram.com/maryumbokhari97/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        Follow me on social media:<br></br>
+                            <a href="https://www.linkedin.com/in/maryum-bokhari-9070581ba/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                            <a href="https://www.instagram.com/maryumbokhari97/" target="_blank"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
                     
                     </Col>
                 </Row>
