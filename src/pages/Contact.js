3b84a929117796eee5e661a76e4dfd24a8870450
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 11:34:33 +0100

    added new files

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Contact() {
+    return (
+        <div>
+            <Navbar />
+            This is the Contact Page!:)
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Portfolio() {
+    return (
+        <div>
+            <Navbar />
+            This is the Portfolio Page!:)
+        </div>
+    )
+}
+
+export default Portfolio;
