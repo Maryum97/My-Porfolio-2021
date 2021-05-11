57874b06970df5b2fcf03877cb177bead2e1582b
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 11:33:40 +0100

    added route to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={About}/>
+        <Route exact path='/porfolio' component={Portfolio}/>
+        <Route exact path='/contact' component={Contact}/>
+      </div>
+    </Router>
   );
 }
 
