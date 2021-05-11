be123f8a4528d0c88599db4d776d83a33b105f47
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-11 15:55:19 +0100

    made Font Awesome icons work

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
@@ -11,8 +12,8 @@ function Footer() {
                     <Col>© 2021 Copyright</Col>
                     <Col>
                         Follow me on social media:<br></br>
-                            <a href="https://www.linkedin.com/in/maryum-bokhari-9070581ba/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                            <a href="https://www.instagram.com/maryumbokhari97/" target="_blank"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
+                            <a href="https://www.linkedin.com/in/maryum-bokhari-9070581ba/" target="_blank"><FontAwesomeIcon icon={ faLinkedin } /></a>
+                            <a href="https://www.instagram.com/maryumbokhari97/" target="_blank"><FontAwesomeIcon icon={ faInstagram } /></a>
                     
                     </Col>
                 </Row>
