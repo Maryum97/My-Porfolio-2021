42a03d9a572653d008e3c438c6d58d1c79b51bac
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-12 06:26:34 +0100

    removed form submission functionality

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,69 +1,51 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
 import { Container, FormGroup, Label, Input } from 'reactstrap';
-import { nodemailer, sendEmail } from 'nodemailer';
 
-function ContactForm() {
-    const [mailerState, setMailerState] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
-
-    function handleStateChange(e) {
-        setMailerState((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
+class ContactForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            emailStatus: '',
+        };
     }
 
-    const submitEmail = async (e) => {
-        e.preventDefault();
-        console.log({ mailerState });
-        const response = await fetch("http://localhost:3001/send", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify({ mailerState }),
-        })
-            .then((res) => res.json())
-            .then(async (res) => {
-                const resData = await res;
-                console.log(resData);
-                if (resData.status === "success") {
-                    alert("Message Sent");
-                } else if (resData.status === "fail") {
-                    alert("Message failed to send");
-                }
-            })
-            .then(() => {
-                setMailerState({
-                    email: "",
-                    name: "",
-                    message: "",
-                });
+    handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+        emailjs.sendForm('gmail', 'template_irxkmod', event.target, 'user_gxiulwpVRO5vyn8yUAmeN')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
             });
-    };
+            event.target.reset();
+    }
 
-    return (
-        <Container>
-            <form onSubmit={sendEmail}>
-                <FormGroup>
-                    <Label for="name">Full Name:</Label>
-                    <Input type="text" name="name" id="name" value={mailerState.name} onChange={handleStateChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email:</Label>
-                    <Input type="email" name="email" id="email" value={mailerState.email} onChange={handleStateChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">Leave A Message:</Label>
-                    <Input type="textarea" name="message" id="message" value={mailerState.message} onChange={handleStateChange} />
-                </FormGroup><br></br>
-                <Input type="submit">Submit </Input>
-            </form>
-        </Container>
-    )
+    render() {
+        return (
+            <Container>
+                <form onSubmit={this.handleFormSubmit}>
+                    <FormGroup>
+                        <Label for="name">Full Name:</Label>
+                        <Input type="text" name="name" id="name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email:</Label>
+                        <Input type="email" name="email" id="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleFile">Leave A Message:</Label>
+                        <Input type="textarea" name="message" id="message" value={this.state.message} onChange={e => this.setState({ message: e.target.value })} />
+                    </FormGroup><br></br>
+                    <Input type="submit" onClick={e => this.handleFormSubmit(e)} value="submit">Submit </Input>
+                </form>
+            </Container>
+        )
+    }
 }
 
 export default ContactForm;
