dded67398ce8b6aaec8ecd269faf5d834d019c09
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-12 03:43:04 +0100

    added form submission functionality

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+import { Container, Button, FormGroup, Label, Input } from 'reactstrap';
 
-function ContactForm() {
-    return (
-        <Form>
-            <FormGroup>
-                <Label for="name">Full Name</Label>
-                <Input type="text" name="name" id="name" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="email">Email</Label>
-                <Input type="email" name="email" id="email" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleFile">Message</Label>
-                <Input type="textarea" name="message" id="message" />
-            </FormGroup>
-            <Button>Submit</Button>
-        </Form>
+class ContactForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            emailStatus: '',
+        };
+    }
 
-    )
+    handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+        emailjs.sendForm('gmail', 'template_irxkmod', event.target, 'user_gxiulwpVRO5vyn8yUAmeN')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            event.target.reset();
+    }
+
+    render() {
+        return (
+            <Container>
+                <form onSubmit={this.handleFormSubmit}>
+                    <FormGroup>
+                        <Label for="name">Full Name:</Label>
+                        <Input type="text" name="name" id="name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email:</Label>
+                        <Input type="email" name="email" id="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleFile">Leave A Message:</Label>
+                        <Input type="textarea" name="message" id="message" value={this.state.message} onChange={e => this.setState({ message: e.target.value })} />
+                    </FormGroup><br></br>
+                    <Button type="submit" onClick={e => this.handleFormSubmit(e)} value="submit">Submit</Button>
+                </form>
+            </Container>
+        )
+    }
 }
 
 export default ContactForm;
