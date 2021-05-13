071881d2c0fb9db0551b036e951fa470e4b5d8cd
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-13 15:34:02 +0100

    added content

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -15,8 +15,23 @@ function Portfolio() {
                 <Container>
                     <Row>
                         <h2>Back in Pakistan...</h2>
-                        <Col>Image 1</Col>
-                        <Col>Image 2</Col>
+                        <Col><img src='../../Assets/Portfolio/LahoreOnAMap.JPG'></img></Col>
+                        <Col><img src='../../Assets/Portfolio/.JPG'></img></Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p>
+                                See that pink star? That's Lahore, my hometown, on a map. It's big, highly advanced and
+                                extremely attractive for people to find greater opportunities, throughout Punjab, Pakistan. I was very
+                                lucky to be raised here :).
+                            </p>
+                        </Col>
+                        <Col>
+                            <p>
+                                Just thought I'd share an image of the very beautiful and famous Shahi Qila, or the Lahore Fort;
+                                an important landmark and tourist attracttion of my city.
+                            </p>
+                        </Col>
                     </Row>
                 </Container>
                 <Footer />
