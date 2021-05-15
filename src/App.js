5805296f4779ab564edad49bf132db0e241d6157
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-15 23:24:13 +0100

    changed routes to switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={About}/>
-        <Route exact path={process.env.PUBLIC_URL + '/portfolio'} component={Portfolio}/>
-        <Route exact path={process.env.PUBLIC_URL + '/contact'} component={Contact}/>
-      </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path={'/'} component={About} ></Route>
+        <Route exact path={'/portfolio'} component={Portfolio} ></Route>
+        <Route exact path={'/contact'} component={Contact} ></Route>
+      </Switch>
     </Router>
   );
 }
